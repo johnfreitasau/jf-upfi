bf8e245b149a78deeccaac9442003fb2d8506308
John Freitas <john.freitasau@gmail.com>
2021-08-17 07:56:53 +1000

    Created the ViewImage Modal component

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,5 +1,6 @@
 import { SimpleGrid, useDisclosure } from '@chakra-ui/react';
 import { useState } from 'react';
+import { Grid, GridItem } from '@chakra-ui/react';
 import { Card } from './Card';
 import { ModalViewImage } from './Modal/ViewImage';
 
@@ -17,23 +18,58 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
+  const { isOpen, onClose, onOpen } = useDisclosure();
 
   // TODO SELECTED IMAGE URL STATE
+  const [selectedImageUrl, setSelectedImageUrl] = useState('');
 
   // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(url: string): void {
+    setSelectedImageUrl(url);
+    onOpen();
+  }
 
   return (
     <>
       {/* TODO CARD GRID */}
-      {cards.map(card => (
-        <>
-          <img src={card.url} alt={card.title} />
-          <h2>{card.title}</h2>
-          {card.description}
-        </>
-      ))}
-
+      <SimpleGrid templateColumns="repeat(3, 1fr)" spacing="10" width="full">
+        {cards.map(card => (
+          <>
+            <Card data={card} viewImage={() => handleViewImage(card.url)} />
+            <Card data={card} viewImage={() => handleViewImage(card.url)} />
+            <Card data={card} viewImage={() => handleViewImage(card.url)} />
+            <Card data={card} viewImage={() => handleViewImage(card.url)} />
+            <Card data={card} viewImage={() => handleViewImage(card.url)} />
+            <Card data={card} viewImage={() => handleViewImage(card.url)} />
+            <Card data={card} viewImage={() => handleViewImage(card.url)} />
+            <Card data={card} viewImage={() => handleViewImage(card.url)} />
+            <Card data={card} viewImage={() => handleViewImage(card.url)} />
+          </>
+        ))}
+      </SimpleGrid>
       {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        isOpen={isOpen}
+        onClose={onClose}
+        imgUrl={selectedImageUrl}
+      />
     </>
   );
 }
+
+/*
+{
+  "title": "Doge",
+  "description": "The best doge",
+  "url": "https://i.ibb.co/K6DZdXc/minh-pham-LTQMgx8t-Yq-M-unsplash.jpg",
+  "ts": 1620222828340000,
+  "id": "294961059684418048"
+},
+{
+  title: "Inspiring",
+  description: "inspiring",
+  url: "https://i.ibb.co/X2rccTV/inspiring01.png",
+  ts: 1620222828340000,
+  id: "294961059684418049"
+}
+*/
--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -6,6 +6,7 @@ import {
   ModalBody,
   Image,
   Link,
+  ModalCloseButton,
 } from '@chakra-ui/react';
 
 interface ModalViewImageProps {
@@ -20,4 +21,31 @@ export function ModalViewImage({
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
   // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal onClose={onClose} isOpen={isOpen} isCentered>
+      <ModalOverlay />
+      <ModalContent
+        borderBottomRightRadius="6"
+        borderBottomLeftRadius="6"
+        overflow="hidden"
+        background="transparent"
+      >
+        <ModalBody>
+          <ModalCloseButton />
+          <Image src={imgUrl} alt={imgUrl} />
+        </ModalBody>
+        <ModalFooter
+          bgColor="pGray.800"
+          p="2"
+          justifyContent="flex-start"
+          alignItems="center"
+        >
+          {' '}
+          <Link href={imgUrl} isExternal>
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
